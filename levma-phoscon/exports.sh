e8aa3b81d6486daf76181bc80a07112ce3e0ac4c
Malte Leverenz <m.leverenz@payergbr.com>
2022-11-12 11:44:06 +0100

    removed uneeded exports

--- levma-phoscon/exports.sh
+++ levma-phoscon/exports.sh
@@ -1,6 +1,1 @@
 export APP_LEVMA_PHOSCON_IP="10.21.0.207"
-export APP_LEVMA_PHOSCON_WEB_PORT="80"
-export APP_LEVMA_PHOSCON_WS_PORT="30443"
-export APP_LEVMA_PHOSCON_VNC_PORT="35900"
-export APP_LEVMA_PHOSCON_NOVNC_PORT="36080"
-export APP_LEVMA_PHOSCON_DEVICE=$(readlink -f /dev/serial/by-id/usb-dresden_elektronik_ingenieurtechnik_GmbH_ConBee_II_DE2475420-if00)
